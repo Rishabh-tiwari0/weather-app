b769dc7f63f306c85d82e2f2872715365105adb8
Rishabh-tiwari0 <htrishabh.tiwari@gmail.com>
2024-04-15 09:52:40 +0530

    css changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,7 +2,7 @@ const Demo = () => {
   return (
     <div className={`flex justify-center items-center flex-col mt-8 -z-10`}>
       <h1 className={`text-white text-3xl font-semibold `}>How to ??</h1>
-      <div className={`w-1/2 mt-4 border-2 border-white rounded-md`}>
+      <div className={`w-10/12 mt-4 border-2 border-white rounded-md sm:w-1/2`}>
         <video src="demo.mkv" autoPlay controls></video>
       </div>
     </div>
